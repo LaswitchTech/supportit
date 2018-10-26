f46b69278820bbcf82200266a7a52a13a8110be1
LaswitchTech <dev@laswitchtech.com>
2018-10-25 21:46:05 -0400

    Thu Oct 25 21:46:05 EDT 2018

--- bin/scheduler.php
+++ bin/scheduler.php
@@ -98,7 +98,7 @@
               //Update log
               $sql = "INSERT INTO logs ( owner, created, modified, type, tbl, content, user_id, ipv4, is_success ) VALUES ( 1, '".$DATE."', '".$DATE."', 1, 'tickets', '".$sql."', 1, '".$_SERVER['REMOTE_ADDR']."', 0 )";
               if ($conn->query($sql) === TRUE) {
-                echo "Log Updated"
+                echo "Log Updated";
               } else {
                 echo "Error: " . $sql . "<br>" . $conn->error;
               }
