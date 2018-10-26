95755159db57568b31c7d6a48d29630bdb8106ea
LaswitchTech <dev@laswitchtech.com>
2018-10-25 22:22:22 -0400

    Thu Oct 25 22:22:22 EDT 2018

--- bin/scheduler.php
+++ bin/scheduler.php
@@ -41,11 +41,14 @@
 
     // Fetch Contact
     $sql = "SELECT * FROM contacts WHERE email = $mail_address";
-    $contact_result = mysqli_query($conn, $sql);
+    echo "#################\n"
+    echo $sql;
+    echo "\n#################\n"
+    $contact_result = $conn->query($sql);
     $contact = mysqli_fetch_assoc($contact_result);
 
     // Verify Existence of User and Verify Mail Issuer for New Ticket
-    if (mysqli_num_rows($result) > 0){
+    if ($contact_result->num_rows > 0){
       if ( $email->subject == "New Message From LaswitchTech" ){
         if ( $email->from == "LaswitchTech <info@laswitchtech.com>" ){
           if ( $email->seen == 0 ){
