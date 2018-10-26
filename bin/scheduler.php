f7acfc997d6f859dc2ab3cea1c608ae69f08c870
LaswitchTech <dev@laswitchtech.com>
2018-10-25 22:35:52 -0400

    Thu Oct 25 22:35:52 EDT 2018

--- bin/scheduler.php
+++ bin/scheduler.php
@@ -53,62 +53,66 @@
         echo "mail_description : ".$mail_description."\n";
         echo "###################################################################\n";
         $contact_result = $conn->query($sql);
+
+        if( $contact_result ){
+
         $contact = mysqli_fetch_assoc($contact_result);
 
-        if ( $contact_result->num_rows > 0 ){
-          if ( $email->seen == 0 ){
-            $sql = "INSERT INTO tickets ( owner, created, modified, account_id, contact_id, state, status, priority, type, subject, description, user_id ) VALUES ( 2, '".$DATE."', '".$DATE."', '".$contact->account_id."', '".$contact->id."', 0, 0, 3, 1, '".$mail_subjet."', '".$mail_description."', 1 )";
+          if ( $contact_result->num_rows > 0 ){
+            if ( $email->seen == 0 ){
+              $sql = "INSERT INTO tickets ( owner, created, modified, account_id, contact_id, state, status, priority, type, subject, description, user_id ) VALUES ( 2, '".$DATE."', '".$DATE."', '".$contact->account_id."', '".$contact->id."', 0, 0, 3, 1, '".$mail_subjet."', '".$mail_description."', 1 )";
 
-            if ($conn->query($sql) === TRUE) {
-              $last_id = $conn->insert_id;
-              # To Email Address
-              $emailaddress=$mail_address;
-              # Message Subject
-              $emailsubject="Ticket#".$last_id." has been created";
-
-              # Common Headers
-              $headers .= 'From: LaswitchTech-Support <support@laswitchtech.com>'.$eol;
-              $headers .= 'Reply-To: LaswitchTech-Support <support@laswitchtech.com>'.$eol;
-              $headers .= 'Return-Path: LaswitchTech-Support <support@laswitchtech.com>'.$eol;     // these two to set reply address
-              $headers .= "Message-ID:<".$DATE." system@laswitchtech.com>".$eol;
-              $headers .= "X-Mailer: PHP v".phpversion().$eol;           // These two to help avoid spam-filters
-              # Boundry for marking the split & Multitype Headers
-              $mime_boundary=md5(time());
-              $headers .= 'MIME-Version: 1.0'.$eol;
-              $headers .= "Content-Type: multipart/related; boundary=\"".$mime_boundary."\"".$eol;
-              $msg = "";
-
-              # Text Version
-              $msg .= "--".$mime_boundary.$eol;
-              $msg .= "Content-Type: text/plain; charset=iso-8859-1".$eol;
-              $msg .= "Content-Transfer-Encoding: 8bit".$eol;
-              $msg .= "This is a multi-part message in MIME format.".$eol;
-              $msg .= "If you are reading this, please update your email-reading-software.".$eol;
-              $msg .= "+ + Text Only Email from Genius Jon + +".$eol.$eol;
-
-              # Finished
-              $msg .= "--".$mime_boundary."--".$eol.$eol;   // finish with two eol's for better security. see Injection.
-
-              # SEND THE EMAIL
-              ini_set(sendmail_from,'support@laswitchtech.com');  // the INI lines are to force the From Address to be used !
-              mail($emailaddress, $emailsubject, $msg, $headers);
-              ini_restore(sendmail_from);
-
-              //Update log
-              $sql = "INSERT INTO logs ( owner, created, modified, type, tbl, content, user_id, ipv4, is_success ) VALUES ( 1, '".$DATE."', '".$DATE."', 1, 'tickets', '".$sql."', 1, '".$_SERVER['REMOTE_ADDR']."', 1 )";
               if ($conn->query($sql) === TRUE) {
-                echo "Log Updated";
+                $last_id = $conn->insert_id;
+                # To Email Address
+                $emailaddress=$mail_address;
+                # Message Subject
+                $emailsubject="Ticket#".$last_id." has been created";
+
+                # Common Headers
+                $headers .= 'From: LaswitchTech-Support <support@laswitchtech.com>'.$eol;
+                $headers .= 'Reply-To: LaswitchTech-Support <support@laswitchtech.com>'.$eol;
+                $headers .= 'Return-Path: LaswitchTech-Support <support@laswitchtech.com>'.$eol;     // these two to set reply address
+                $headers .= "Message-ID:<".$DATE." system@laswitchtech.com>".$eol;
+                $headers .= "X-Mailer: PHP v".phpversion().$eol;           // These two to help avoid spam-filters
+                # Boundry for marking the split & Multitype Headers
+                $mime_boundary=md5(time());
+                $headers .= 'MIME-Version: 1.0'.$eol;
+                $headers .= "Content-Type: multipart/related; boundary=\"".$mime_boundary."\"".$eol;
+                $msg = "";
+
+                # Text Version
+                $msg .= "--".$mime_boundary.$eol;
+                $msg .= "Content-Type: text/plain; charset=iso-8859-1".$eol;
+                $msg .= "Content-Transfer-Encoding: 8bit".$eol;
+                $msg .= "This is a multi-part message in MIME format.".$eol;
+                $msg .= "If you are reading this, please update your email-reading-software.".$eol;
+                $msg .= "+ + Text Only Email from Genius Jon + +".$eol.$eol;
+
+                # Finished
+                $msg .= "--".$mime_boundary."--".$eol.$eol;   // finish with two eol's for better security. see Injection.
+
+                # SEND THE EMAIL
+                ini_set(sendmail_from,'support@laswitchtech.com');  // the INI lines are to force the From Address to be used !
+                mail($emailaddress, $emailsubject, $msg, $headers);
+                ini_restore(sendmail_from);
+
+                //Update log
+                $sql = "INSERT INTO logs ( owner, created, modified, type, tbl, content, user_id, ipv4, is_success ) VALUES ( 1, '".$DATE."', '".$DATE."', 1, 'tickets', '".$sql."', 1, '".$_SERVER['REMOTE_ADDR']."', 1 )";
+                if ($conn->query($sql) === TRUE) {
+                  echo "Log Updated";
+                } else {
+                  echo "Error: " . $sql . "<br>" . $conn->error;
+                }
               } else {
                 echo "Error: " . $sql . "<br>" . $conn->error;
-              }
-            } else {
-              echo "Error: " . $sql . "<br>" . $conn->error;
-              //Update log
-              $sql = "INSERT INTO logs ( owner, created, modified, type, tbl, content, user_id, ipv4, is_success ) VALUES ( 1, '".$DATE."', '".$DATE."', 1, 'tickets', '".$sql."', 1, '".$_SERVER['REMOTE_ADDR']."', 0 )";
-              if ($conn->query($sql) === TRUE) {
-                echo "Log Updated";
-              } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
+                //Update log
+                $sql = "INSERT INTO logs ( owner, created, modified, type, tbl, content, user_id, ipv4, is_success ) VALUES ( 1, '".$DATE."', '".$DATE."', 1, 'tickets', '".$sql."', 1, '".$_SERVER['REMOTE_ADDR']."', 0 )";
+                if ($conn->query($sql) === TRUE) {
+                  echo "Log Updated";
+                } else {
+                  echo "Error: " . $sql . "<br>" . $conn->error;
+                }
               }
             }
           }
